603303d2756bec2677c88a02df7828682b2373c9
ranji1221 <jiran1221@163.com>
2016-09-07 10:50:22 +0800

    fiexed messy code item.php in frontend/view/post

--- frontend/views/post/item.php
+++ frontend/views/post/item.php
@@ -1,11 +1,11 @@
 <?php
-	$this->title = $post->title;
+	$this->title = $post->title; 
 	$this->params['breadcrumbs'][] = ['label'=>'Post','url'=>['index']];
 	$this->params['breadcrumbs'][] = $this->title;
 ?>
 
 <h1><?php echo $post->title; ?></h1>
-<p class="text-muted">??:<?php echo $post->author; ?></p>
+<p class="text-muted">作者：<?php echo $post->author; ?></p>
 <p>
 	<?php echo yii\helpers\Markdown::process($post->content,'gfm');?>
 </p>
